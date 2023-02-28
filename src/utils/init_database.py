a8514874cc8e5619197876a1ca7d2195a5367c3a
vateseif <vateseif@gmail.com>
2023-02-28 20:35:49 +0100

    create database with correct evaluatio/mate

--- src/utils/init_database.py
+++ src/utils/init_database.py
@@ -1,48 +1,54 @@
 import sqlite3
+import chess
 import chess.pgn
 
 # Connect to the database
 conn = sqlite3.connect('test.db')
 cursor = conn.cursor()
 
-"""
+
 # Create the table
 cursor.execute('''CREATE TABLE games
                   (id INTEGER PRIMARY KEY,
                    fen TEXT,
+                   mate BIT,
                    eval FLOAT);''')
-"""
+
 
 # Open the PGN file and read the games
 with open('src/utils/game.pgn') as pgn_file:
     game = chess.pgn.read_game(pgn_file)
 
-
     n_move = 0
     # Loop through each game in the PGN file
     while game is not None:
-        board = game.board()
+      board = game.board()
 
-        # Loop through each move in the game
-        for move in game.mainline_moves():
-            board.push(move)
-            fen = board.fen()
-
+      for node in game.mainline():
             
-            #score = board.pop().eval()
-            print(move)
-            # Get the evaluation for the current board state
-            # Replace this with your own evaluation function
-            #eval = evaluate_board(board)
+        evaluation = node.eval()
+        if evaluation is None: break
 
-            # Insert the data into the database
-            #cursor.execute("INSERT INTO games (id, fen, eval) VALUES (?, ?, ?)",
-            #               (n_move, fen, 0))
+        board.push(node.move)
+        fen = board.fen()
 
-            n_move += 1
+        int_score = node.eval().white()
+        is_mate = int_score.is_mate() 
+        evaluation = 0
+        # if it is mate in n moves, evalaute is the number of moves
+        if is_mate:
+          evaluation = int_score.mate()
+        else:
+          evaluation = int_score.score()
 
-        # Read the next game in the PGN file
-        game = chess.pgn.read_game(pgn_file)
+        # Insert the data into the database
+        cursor.execute("INSERT INTO games (id, fen, mate, eval) VALUES (?, ?, ?, ?)",
+                      (n_move, fen, int(is_mate), evaluation))
+
+        n_move += 1
+
+      # Read the next game in the PGN file
+      game = chess.pgn.read_game(pgn_file)
 
 # Commit the changes and close the connection
 conn.commit()
