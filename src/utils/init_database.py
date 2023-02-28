74f28fb997ef1f7b206cfeb82162beb8f8147c1b
vateseif <vateseif@gmail.com>
2023-02-28 21:32:43 +0100

    fix script to store data in db

--- src/utils/init_database.py
+++ src/utils/init_database.py
@@ -1,9 +1,14 @@
-import sqlite3
 import chess
+import logging
+import sqlite3
 import chess.pgn
 
+#init logging
+logging.basicConfig(filename="init_db.log", level=logging.INFO, 
+      format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+
 # Connect to the database
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('lichess_id_fen_mate_eval_2022-06.db')
 cursor = conn.cursor()
 
 
@@ -16,34 +21,41 @@ cursor.execute('''CREATE TABLE games
 
 
 # Open the PGN file and read the games
-with open('src/utils/game.pgn') as pgn_file:
+with open('src/utils/lichess_db_standard_rated_2022-06.pgn') as pgn_file:
     game = chess.pgn.read_game(pgn_file)
-
-    n_move = 0
+    n_game = 0
+    n_move = 0 # keep track of moves stored
     # Loop through each game in the PGN file
     while game is not None:
+      added_game = False
       board = game.board()
-
       for node in game.mainline():
-            
+        # Check that evaluation was saved for this game else skip
         evaluation = node.eval()
         if evaluation is None: break
+        
+        # increase game counter
+        if not added_game:
+          n_game += 1
+          added_game = True
 
         board.push(node.move)
         fen = board.fen()
 
-        int_score = node.eval().white()
-        is_mate = int_score.is_mate() 
+        score = node.eval().white()
+        is_mate = score.is_mate() 
         evaluation = 0
         # if it is mate in n moves, evalaute is the number of moves
         if is_mate:
-          evaluation = int_score.mate()
+          evaluation = score.mate()
         else:
-          evaluation = int_score.score()
+          evaluation = score.score()
 
         # Insert the data into the database
         cursor.execute("INSERT INTO games (id, fen, mate, eval) VALUES (?, ?, ?, ?)",
                       (n_move, fen, int(is_mate), evaluation))
+        # log
+        logging.info(f"logged {n_move}ith move. Currently in {n_game}th game")
 
         n_move += 1
 
